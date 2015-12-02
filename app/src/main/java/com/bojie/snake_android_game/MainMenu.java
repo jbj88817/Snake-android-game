703181127787ec8b37f2f264f0f3a258d238ce11
Bojie Jiang <jiangbojie@gmail.com>
2015-12-02 00:17:14 -0800

    Add images and create MainMenu layout

--- app/src/main/java/com/bojie/snake_android_game/MainMenu.java
+++ app/src/main/java/com/bojie/snake_android_game/MainMenu.java
@@ -9,12 +9,12 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_menu);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
