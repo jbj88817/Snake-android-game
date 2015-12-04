da8f24789a9a3fc9a0c5192c79b7918f03e0b11c
Bojie Jiang <jiangbojie@gmail.com>
2015-12-03 21:07:47 -0800

    MainMenu game code and initClassic method

--- app/src/main/java/com/bojie/snake_android_game/MainMenu.java
+++ app/src/main/java/com/bojie/snake_android_game/MainMenu.java
@@ -1,52 +1,76 @@
 package com.bojie.snake_android_game;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 
 public class MainMenu extends AppCompatActivity {
+
+    private RelativeLayout mSnakeLayout;
+    private Animation mCompileAnim;
+    private AdView mAdView;
+    private ImageView mClassicBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        mSnakeLayout = (RelativeLayout) findViewById(R.id.snake_layout);
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
         }
 
-        return super.onOptionsItemSelected(item);
+        mAdView = new AdView(this);
+        mAdView.setAdSize(AdSize.SMART_BANNER);
+        mAdView.setAdUnitId(GameSettings.MY_AD_UNIT_ID);
+        mSnakeLayout.addView(mAdView);
+
+        AdRequest adRequest = new AdRequest.Builder().addTestDevice(AdRequest.DEVICE_ID_EMULATOR).build();
+        mAdView.loadAd(adRequest);
     }
+
+    private void initClassic() {
+        mClassicBtn = (ImageView) findViewById(R.id.classic);
+        mCompileAnim = AnimationUtils.loadAnimation(MainMenu.this, R.anim.anim_for_classic_button);
+        mCompileAnim.setDuration(GameSettings.ANIMATION_OPEN_BUTTON_DURATION);
+        mCompileAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                mClassicBtn.setImageResource(R.mipmap.classic);
+                mClassicBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intentClassic = new Intent(MainMenu.this, ClassicSnake.class);
+                        intentClassic.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                        startActivity(intentClassic);
+                    }
+                });
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        mClassicBtn.startAnimation(mCompileAnim);
+    }
+
 }
